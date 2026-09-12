0e5ca0c0a98114092ae6b4e6a4c6c43f35f317ca
Santiago Alfred <santiago.alfred@ucb.edu.bo>
2026-09-12 17:49:48 -0400

    Guarda en variables, el botón y sus 2 íconos y textos y define también el nombre de la clave que se usará para guardar el tema

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   inicializar_tema();
   inicializar_validacion_formulario();
 });
+
+/*1. Modo claro y oscuro*/
+function inicializar_tema() {
+  const html = document.documentElement;
+  const boton_tema = document.getElementById('theme_toggle');
+  const icono_tema = document.getElementById('theme_toggle_icon');
+  const texto_tema = document.getElementById('theme_toggle_text');
+
+  const CLAVE_ALMACENAMIENTO = 'santiago_cv_tema';
+}
